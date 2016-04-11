bfcf80d336d61ef3163a1d0a8ee36337cb4e1022
tonyynot <alramella@gmail.com>
2016-04-10 21:00:03 -0400

    initialize repo

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,48 @@
+class AlbumsController < ApplicationController
+	before_action :find_album, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@albums = Album.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@album = Album.new
+	end
+
+	def create
+		@album = Album.new(album_params)
+		if @album.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @album.update(album_params)
+			redirect_to album_path(@album)
+		else
+			render 'edit'
+		end 
+	end
+
+	def destroy
+	end
+
+private
+
+	def album_params
+		params.require(:album).permit(:title, :description, :artist)
+	end
+
+	def find_album
+		@album = Album.find(params[:id])
+	end
+
+end
